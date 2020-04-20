6e8ef69816ea3029a6e42f92840ac3c432cc71b3
孟刘 <171626037@qq.com>
2020-04-20 16:00:48 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        home
+        <el-button type="info" @click="logout">
+            退出登录
+        </el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
