c0d9fc284664d7a324791d658fb6d8ee5dbef1f8
孟刘 <171626037@qq.com>
2020-05-07 21:32:44 +0800

    数据报表

---
+++ src/components/report/Report.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 2. 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+      <div id="main" style="width: 800px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+// 1. 引包
+import echarts from 'echarts'
+export default {
+  data() {
+    return {
+      // 需要合并的选项
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  created() {},
+  async mounted() {
+    // 3. 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取数据列表失败')
+    }
+    this.$message.success('获取数据列表成功')
+    // 4. 指定图表的配置项和数据
+    const result = Object.assign(this.options, res.data)
+
+    // 5. 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result)
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
